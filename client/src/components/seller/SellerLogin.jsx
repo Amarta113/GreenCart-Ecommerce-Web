c7a212d754717c52f0d011de2027e912d6d848f9
Amarta113 <amartawaghani@gmail.com>
2025-11-26 21:53:58 +0500

    Initialized seller page

---
+++ client/src/components/seller/SellerLogin.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../../context/AppContext'
+
+export default function SellerLogin(){
+    const {isSeller, setIsSeller, navigate} = useAppContext()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const onSubmitHandler = async(event) => {
+        event.preventDefault()
+        setIsSeller(true)
+    }
+
+    useEffect(() => {
+        if(isSeller){
+            navigate("/seller")
+        }
+    }, [isSeller])
+    
+    return !isSeller && (
+        <form onSubmit={onSubmitHandler}></form>
+    )
+}
