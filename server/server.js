3d349f81b1cf44d419923b71c0bcf917af988ec2
Amarta113 <amartawaghani@gmail.com>
2025-12-01 18:03:36 +0500

    feat: configure local MongoDB connection with mongoose

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+await connectDB()
+
 // Allow multiple origins
 const allowedOrigins = ['http://localhost:5173/']
 
-//Middleware
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, Credential: true}));
-
 
 app.get('/', (req, res) => res.send(
     "API is working"
