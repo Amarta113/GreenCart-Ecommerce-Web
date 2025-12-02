f556dac825749c414a741a5f47804d7221e56cb7
Amarta113 <amartawaghani@gmail.com>
2025-12-02 22:02:55 +0500

    feat: added login api

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,4 @@
-import { jwt } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 
@@ -8,7 +8,7 @@ export const register = async(req, res) => {
         const {name, email, password} = req.body;
         if(!name || !email || !password){
             return res.json({success:false, message: "Missing Details"})
-        }
+        }  
         const existingUser = await User.findOne({email})
         if(existingUser){
             return res.json({success: false, message: 'User already exists'})
@@ -24,6 +24,41 @@ export const register = async(req, res) => {
         })
         return res.json({success: true, user: {email: user.email, name: user.name}})
     } catch(error){
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Login user: /api/user/login
+export const login = async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.json({success: false, message: "Email and password are required."});
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.json({success: false, message: "Email and password are required."});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch){
+            return res.json({success: false, message: "Email and password are required."});
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000, 
+        })
+        return res.json({success: true, user: {email: user.email, name: user.name}})
+    }
+
+    catch (error){
         console.log(error.message)
         res.json({success: false, message: error.message})
     }
