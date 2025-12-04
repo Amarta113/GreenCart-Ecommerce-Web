11ee0bf62e3c806d42e57f3a5a59591fe41dbd8f
Amarta113 <amartawaghani@gmail.com>
2025-12-04 22:12:05 +0500

    feat: added user authentication check endpoint

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -63,3 +63,16 @@ export const login = async(req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+// Check auth: 
+
+export const isAuth = async(req, res) => {
+    try{
+        const {userId} = req.body;
+        const user = await User.findById(userId).select("-password")
+        return res.json({success: true, user})
+    } catch(error){
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
