1ce365f5bfc5e14fac351ccacd9052fbbfaacb07
Amarta113 <amartawaghani@gmail.com>
2025-12-05 21:36:08 +0500

    initialized product api checkpoints

---
+++ server/controller/productController.js
@@ -0,0 +1,20 @@
+// Add product: /api/product/add
+export const addProduct = async(req, res) => {
+
+}
+
+// Get product
+export const productList = async(req, res) => {
+
+}
+
+
+// Get single product: 
+export const productById = async(req, res) => {
+
+}
+
+// change product inStock
+export const changeStock = async(req, res) =>{
+
+}
