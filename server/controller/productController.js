ed4c971ee304600dcb884962ce55acc16f392183
Amarta113 <amartawaghani@gmail.com>
2025-12-06 13:55:34 +0500

    added controller to get single product by id

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -35,7 +35,14 @@ export const productList = async(req, res) => {
 
 // Get single product: 
 export const productById = async(req, res) => {
-
+    try{
+        const {id} = req.body
+        const product = await Product.findById(id)
+        res.json({success: true, product})
+    } catch(error){
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
 }
 
 // change product inStock
