7f9a3d11d963e9a7bdec05a7230b9d5dda47fb87
Amarta113 <amartawaghani@gmail.com>
2025-12-06 13:59:16 +0500

    set controller to update product in stock

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -47,5 +47,12 @@ export const productById = async(req, res) => {
 
 // change product inStock
 export const changeStock = async(req, res) =>{
-
+    try{
+        const {id, inStock} = req.body
+        await Product.findByIdAndUpdate(id, {inStock})
+        res.json({success:true, message: "Stock Updated"})
+    } catch(error){
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
 }
