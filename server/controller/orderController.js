0e5c826d827a3968beb3c6ded42fd06ffc29462c
Amarta113 <amartawaghani@gmail.com>
2025-12-07 11:00:50 +0500

    added sorting in getallorders

--- server/controller/orderController.js
+++ server/controller/orderController.js
@@ -50,7 +50,7 @@ export const getAllOrders = async(req, res) => {
     try{
         const orders = await Order.find({
             $or: [{paymentType: "COD"}, {isPaid: true}]
-        }).populate("items.product address")
+        }).populate("items.product address").sort({createdAt: -1})
         res.json({success: true, orders})
     } catch(error) {
         res.json({success: false, message: error.message})
