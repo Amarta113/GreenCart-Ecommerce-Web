4de813800e751d486e126f7ea8888892ae7f83ad
Amarta113 <amartawaghani@gmail.com>
2025-12-11 22:04:18 +0500

    added error handling middleware

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import { errorMiddleware } from './middlewares/error.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -24,7 +25,8 @@ const allowedOrigins = ['http://localhost:5173']
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: allowedOrigins, 
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true}));
 
 app.get('/', (req, res) => res.send("API is working"))
@@ -36,6 +38,7 @@ app.use('/api/cart', cartRouter)
 app.use('/api/address', addressRouter)
 app.use('/api/order', orderRouter)
 
+app.use(errorMiddleware)
 app.listen(port, () => (
     console.log(`server is running on http://localhost:${port}`)
 ))
