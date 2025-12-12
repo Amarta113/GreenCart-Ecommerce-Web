50f07cae61401534b24519c958d456fb9f7c163f
Amarta113 <amartawaghani@gmail.com>
2025-12-12 21:11:19 +0500

    fixed userId in addaddress

--- server/controller/addressController.js
+++ server/controller/addressController.js
@@ -4,7 +4,9 @@ import Address from "../models/Address.js"
 
 export const addAddress = async(req, res) => {
     try{
-        const {address, userId} = req.body
+        const {address} = req.body
+        // authUser middleware sets req.userId from the JWT
+        const userId = req.userId
         await Address.create({...address, userId})
         res.json({success: true, message: "address added successfully"})
     } catch(error){
@@ -17,9 +19,9 @@ export const addAddress = async(req, res) => {
 // get address: /api/address/get
 export const getAddress = async(req, res) => {
     try {
-        const {userId} = req.body
-        const address = await Address.find({userId})
-        res.json({success: true, message: "Address added successfully"})
+        const userId = req.userId
+        const addresses = await Address.find({userId})
+        res.json({success: true, addresses})
     } catch(error) {
         console.log(error.message)
         res.json({success:false, message: error.message})
