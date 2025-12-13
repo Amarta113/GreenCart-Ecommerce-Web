e2bd3cf7d1a78994277c8eef741a4574545d9cdb
Amarta113 <amartawaghani@gmail.com>
2025-12-13 17:07:38 +0500

    added loader route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import SellerLayout from './pages/Seller/SellerLayout'
 import AddProduct from './pages/Seller/AddProduct'
 import ProductList from './pages/Seller/ProductList'
 import Orders from './pages/Seller/Orders'
+import Loading from './components/Loading'
 
 export default function App(){
   const isSellerPath = useLocation().pathname.includes("seller")
@@ -37,6 +38,7 @@ export default function App(){
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/add-address' element={<AddAddress/>}/>
         <Route path='/my-orders' element={<MyOrders/>}/>
+        <Route path='/loader' element={<Loading/>}/>
         <Route path='/seller' element={isSeller? <SellerLayout/> : <SellerLogin/>}>
           <Route index element={isSeller? <AddProduct/> : null}/>
           <Route path='product-list' element={<ProductList/>}/>
